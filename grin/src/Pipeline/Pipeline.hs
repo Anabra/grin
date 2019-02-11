bf3e79e5ac8a55a8857799c6d4e9f7b1cba05646
Anabra <peter.d.podlovics@gmail.com>
2019-02-11 01:49:17 +0100

    Bugfix for Dead Variable Elimination
    Now DVE correctly handles binding patterns.

--- grin/src/Pipeline/Pipeline.hs
+++ grin/src/Pipeline/Pipeline.hs
@@ -788,6 +788,8 @@ randomPipelineM seed = do
     runNameIntro = void . pipelineStep $ Pass
       [ T ProducerNameIntroduction
       , T BindNormalisation
+      , T BindingPatternSimplification
+      , T BindNormalisation
       ]
 
     -- cleanup after producer name intro
@@ -976,6 +978,8 @@ optimizeWithM pre trans post = do
               [ CopyPropagation
               , SimpleDeadVariableElimination
               , ProducerNameIntroduction
+              , BindNormalisation
+              , BindingPatternSimplification
               , BindNormalisation
               , UnitPropagation
               ]
